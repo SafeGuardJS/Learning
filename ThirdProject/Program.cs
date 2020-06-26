26ffd340901b043b76453a3e07e54f75954a783a
Andrey Berezin <ay.berez@gmail.com>
2020-06-26 15:56:05 +0300

    Added ThirdProject

---
+++ ThirdProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ThirdProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
