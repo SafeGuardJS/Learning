032a1438acf9b611fbd9fc10aa57bca7f3da84f4
Andrey Berezin <ay.berez@gmail.com>
2020-06-30 22:00:22 +0300

    Third project is done

--- ThirdProject/Program.cs
+++ ThirdProject/Program.cs
@@ -7,12 +7,34 @@ namespace ThirdProject
         static void Main(string[] args)
         {
             (double x, double y) point;
-            double x, y;
-
-            Console.Write("Enter cooreds of point \n x: ");
+            Console.Write("Enter coords of point  for var №9\n x: ");
             point.x = Convert.ToDouble(Console.ReadLine());
-            Console.Write(" y: ");         
-            point.y = Convert.ToDouble(Console.ReadLine());         
+            Console.Write(" y: ");
+            point.y = Convert.ToDouble(Console.ReadLine());
+
+            if (point.x <= 0 && point.y <= 0 && (Math.Pow(point.x, 2) + Math.Pow(point.y, 2)) <= 4
+               || point.x >= 1 && Math.Pow(point.x, 2) >= point.y && (Math.Pow(point.x, 2) + Math.Pow(point.y, 2)) <= 4)
+            {
+                Console.WriteLine("Point belongs to the area");
+            }
+            else
+            {
+                Console.WriteLine("Point doesn't belong to the area");
+            }
+            Console.Write("Enter coords of point  for var №10\n x: ");
+            point.x = Convert.ToDouble(Console.ReadLine());
+            Console.Write(" y: ");
+            point.y = Convert.ToDouble(Console.ReadLine());
+
+            if (point.y >= 2.5F && point.y <= 0 && Math.Pow(point.x, 2) + Math.Pow(point.y, 2) <= 4
+                || point.x <= 3 && point.y >= 0 && point.y <= 2.5F && Math.Pow(point.x, 2) + Math.Pow(point.y, 2) >= 4)
+            {
+                Console.WriteLine("Point belongs to the area");
+            }
+            else
+            {
+                Console.WriteLine("Point doesn't belong to the area");
+            }
         }
     }
 }
