914b420d1e155fa5add3e07852351f71cc60e991
Andrey Berezin <ay.berez@gmail.com>
2020-06-30 22:20:39 +0300

    Refreshed gitignore & added FourthProject

---
+++ FourthProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FourthProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
