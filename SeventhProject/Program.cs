06cb49eaf8ff6a782216c6df29cd79b48fb231a3
Andrey Berezin <ay.berez@gmail.com>
2020-09-12 02:29:10 +0300

    SeventhProject modified

--- SeventhProject/Program.cs
+++ SeventhProject/Program.cs
@@ -6,7 +6,22 @@ namespace SeventhProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Account account1 = new Account(150);
+            Account account2 = new Account(1000);
+
+            Console.WriteLine(Account.bonus);
+        }
+
+        static public int test { get; set; } = 5;
+    }
+
+    class Account
+    {
+        public static decimal bonus = 100;
+        public decimal totalSum;
+        public Account(decimal sum)
+        {
+            totalSum = sum + bonus;
         }
     }
 }
