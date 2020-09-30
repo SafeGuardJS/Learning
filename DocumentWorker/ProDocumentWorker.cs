75c4d9440c6727f8952c3469d674f6888b41ab29
Andrey Berezin <ay.berez@gmail.com>
2020-09-30 07:49:05 +0300

    DocumentWorker project is done

---
+++ DocumentWorker/ProDocumentWorker.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DocumentWorker 
+{
+    class ProDocumentWorker : DocumentWorker
+    {
+        public override void EditDocument()
+        {
+            Console.WriteLine("Документ отредактирован");
+        }
+        public override void SaveDocument()
+        {
+            Console.WriteLine("Документ сохранен в старом формате, сохранение в остальных форматах доступно в версии Эксперт");
+        }
+    }
+}
---
+++ DocumentWorker/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DocumentWorker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DocumentWorker doc = null;
+
+            switch (Console.ReadLine())
+            {
+                case "exp":
+                    doc = new ExpertDocumentWorker();
+                    break;
+                case "pro":
+                    doc = new ProDocumentWorker();
+                    break;
+                default:
+                    doc = new DocumentWorker();
+                    break;
+            }
+        }
+    }
+}
