76b19ef9b073af2058f11b0695b8ad0069199378
Andrey Berezin <ay.berez@gmail.com>
2020-10-11 18:17:33 +0300

    Indexers modified

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -6,7 +6,23 @@ namespace FirstProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            StaticFirst.Message();
+        }
+
+        static class StaticFirst
+        {
+            static int number = 4;
+
+            static StaticFirst()
+            {
+                Console.WriteLine("Static constructor!");
+                number = 5;
+            }
+
+            public static void Message()
+            {
+                Console.WriteLine("First message!");
+            }
         }
     }
 }
