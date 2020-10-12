7f69121f1f70b7f6324db7fae05bfa48283fe131
Andrey Berezin <ay.berez@gmail.com>
2020-10-12 14:57:21 +0300

    Delegation project is done

---
+++ Delegation/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Delegation
+{
+    static class FindAndReplaceManager
+    {
+        public static void FindNext(string someStr)
+        {
+            Book randBook = new Book();
+            randBook.FindNext(someStr);
+        }
+
+        public static void CreateNote(string str)
+        {
+            Book.Notes randBook = new Book.Notes();
+            randBook.CreateNote(str);
+        }
+    }
+
+    class Book
+    {
+        public class Notes
+        {
+            int notesCounter = 0;
+
+            public void CreateNote(string str)
+            {
+                this.notesCounter++;
+                Console.WriteLine($"Создана {notesCounter} заметка: " + str);
+            }
+        }
+
+        public void FindNext(string str)
+        {
+            Console.WriteLine("Поиск строки: " + str);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FindAndReplaceManager.FindNext("Great job!");
+            FindAndReplaceManager.CreateNote("Lovely quote.");
+        }
+    }
+}
