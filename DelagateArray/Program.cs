82c17376ef66de4374c68fadf00861f09a51dabb
Andrey Berezin <ay.berez@gmail.com>
2020-10-21 20:11:23 +0300

    DelagateArray project is done

---
+++ DelagateArray/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace DelagateArray
+{
+    class Program
+    {
+        static public int GetRand()
+        {
+            return new Random().Next(0, 101);
+        }
+
+        public delegate int RandomNum();
+        public delegate double CalculateAvarage(params RandomNum[] values);
+         
+        static void Main(string[] args)
+        {
+            RandomNum num = Program.GetRand;
+
+            CalculateAvarage avarage = values =>
+            {
+                double sum = 0;
+                double key;
+
+                for (int i = 0; i < values.Length; i++)
+                {
+                    key = values[i]();
+                    Console.Write(key + " ");
+                    sum += key;
+                }
+
+                Console.WriteLine();
+                return sum / values.Length;
+            };
+
+            Console.WriteLine(avarage(num, num, num, num, num));
+        }
+    }
+}
