d2a9d91f5e64173bb09801b162af094eee731e28
SafeGuardJs <ay.berez@gmail.com>
2020-10-30 17:50:11 +0200

    ArrList project done

---
+++ ArrList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ArrList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestClass testClass = new TestClass();
+
+            ArrayList testList = new ArrayList();
+
+            testList.Add(1);
+            testList.Add(testClass);
+
+            Console.WriteLine(testList.Count);
+        }
+    }
+
+    class TestClass
+    {
+
+    }
+}
