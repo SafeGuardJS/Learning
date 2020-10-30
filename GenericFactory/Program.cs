6ea858de3d7dffd8ff7a3da4799df8d93d6428e0
SafeGuardJs <ay.berez@gmail.com>
2020-10-30 17:57:31 +0200

    GenericFactory done

---
+++ GenericFactory/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GenericFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program absurd = MyClass<Program>.FactoryMethod();
+        }
+    }
+
+    class MyClass<T> where T : new()
+    {
+        public static T FactoryMethod()
+        {
+            return new T();
+        }
+    }
+}
